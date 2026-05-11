3300cd882a169a2af77002afb3ff3aa2bd189b95
Paulus <pwilhem@mtc.com.na>
2026-05-11 16:20:21 +0200

    Express

--- sever.js
+++ sever.js
@@ -38,7 +38,7 @@ const PORT = process.env.PORT || 4004;
 sequelize.authenticate()
   .then(() => {
     console.log('Database connected successfully');
-    return sequelize.sync({ alter: true }); 
+    return sequelize.sync({ alter: false }); 
   })
   .then(() => {
     console.log('Database tables synchronized');
